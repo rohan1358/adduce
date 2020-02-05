f745f6c603f685dcaa3f3a588cd5b0bb37621cb8
rohan priyatna <691rohan@gmail.com>
2020-02-06 01:13:35 +0700

    hello world

--- src/models/order.js
+++ src/models/order.js
@@ -57,29 +57,7 @@ module.exports={
 
 
 
-    // insertOrder: (idProduct, quant) => {
-
-
-
-        // return new Promise((resolve, reject) =>{
-        //     let hasil =connecttion.query("SELECT * FROM product WHERE id =?", idProduct)
-        //             connecttion.query("UPDATE product SET stock=? WHERE id = ?", [quant, idProduct], (err, result) => {
-        //                 if(!err){
-        //                    resolve(result);
-        //                 }else{
-        //                     reject(new Error(err));
-        //                 }
-        //             })
-                
-        //     connecttion.query("INSERT INTO tbl_order SET ?", quant,(err, result) => {
-        //         if(!err){
-        //             connecttion.query("UPDATE product SET stock(SELECT (product.stock-tbl_order.qty) FROM product INNER JOIN tbl_order on product.id=tbl_order.product_id) WHERE id = ?", idProduct)
-        //         }else{
-        //             reject(new Error(err));
-        //         }
-        //     })
-        // })
-    // },
+   
     sortOrder: () => {
         return new Promise((resolve, reject) => {
             connecttion.query("SELECT * FROM tbl_order", (err, result) => {
--- src/models/product.js
+++ src/models/product.js
@@ -99,19 +99,8 @@ module.exports={
     // INI ADALAH FUNGSI YANG DI PANGGIL OLEH CONTROLLER DAN DI BERIKAN DATA PRODUCT ID DAN STOK
     updateStokProduct : (product_id, stok) => {
 
-        // KITA INGAT TADI PRODUCT ID MENGANDUNG ANGKA YAITU 1 DAN STOK MENGANDUNG ANGKA 5 SESUAI DATA YANG DI KIRIM POSTMAN
-
-        // 1. KITA HARUS TAU QUERY UNTUK UPDATE 
-        //  *CONTOH : UPDATE product SET stok='5' WHERE id = '1'
-        // 'UPDATE' ADALAH UNTUK MELAKUKAN PRUBAHAN, 'product' ADALAH TABLE YANG AKAN KITA RUBAH ISI DATANYA, 'SET' ADALAH UNTUK MERESET DATA, 'stok' ADALAH NAMA KOLOM YANG AKAN KITA RUBAH, '=5' ADALAH ANGKA STOK YANG AKAN DIRUBAH, 'WHERE' DIMANA YANG AKAN KITA RUBAH, 'id' ADALAH NAMA KOLOM KUNCI DARI PRODUCT, '=1' ADALAH KUNCI DARI DATA YANG AKAN KITA UPDATE 
-
-        // return ADALAH UNTUK MEMBALIKAN PROSES KE CONTROLLER
+      
         return new Promise((resolve, reject) => {
-            // STEP DIBAWAH INI ADALAH STEP PROSES UPDATENYA 
-            // UPDATE product SET stock=? WHERE id=?
-            //  * '?' urutan tanda tanya adalah urutan array, paling depan adalah array ke 0, kedua adalah 1 dst...
-            //  *  dari urutan array tanda tanya di atas dipasangkan dengan param ini [stok,product_id]
-            //  * maka akan menghasilkan  UPDATE product SET stok='5' WHERE id = '1', karnea variable stok sudah di devinisikan dari awal berdasarkan data dari postman yaitu 5 dan id dari product id=1
             connecttion.query("UPDATE product SET stock=? WHERE id=?", [stok,product_id], (err, result) => {
                 if(!err){
                     resolve(result);
